c9af4dbe411037132f9caa262010a64d982f8f57
arun3688 <rain100falls@gmail.com>
2022-04-03 13:24:14 +0200

    do get/set FMUState on other data types

--- OMCompiler/SimulationRuntime/fmi/export/openmodelica/fmu2_model_interface.h
+++ OMCompiler/SimulationRuntime/fmi/export/openmodelica/fmu2_model_interface.h
@@ -104,7 +104,10 @@ typedef struct {
 
 typedef struct {
   RINGBUFFER* simulationData;
-  modelica_real* realParameters;
+  modelica_real* realParameter;
+  modelica_integer* integerParameter;
+  modelica_boolean* booleanParameter;
+  modelica_string* stringParameter;
 } INTERNAL_FMU_STATE;
 
 
